1dd336613d52953e01f1afdbe91b3d55bda9c61e
pramod <pramodreddy.kunreddy@publicissapient.com>
2024-02-14 15:08:25 +0530

    inored java file

--- s.java
+++ s.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class s {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("welcome");
     }
 }
